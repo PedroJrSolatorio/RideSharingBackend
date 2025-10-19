58be8a74699d6f691b7f3fda22c9f3fa25eff188
Pedro Jr Solatorio <solatoriop@gmail.com>
2025-10-19 15:54:33 +0800

    Fix typo in require statement

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const cloudinary = reqiure('cloudinary').v2;
+const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
 const app = express();
